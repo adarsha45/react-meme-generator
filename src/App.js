d9cd185c11bf234633f4da8e2398999f3c0881da
oboe_ada <shresthaadarsha957@gmail.com>
2022-01-25 18:35:32 +0545

    meme generated succesfully for mobile view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Meme from './components/Meme';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
